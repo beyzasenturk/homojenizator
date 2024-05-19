dbc35eec034a77b0c49ae260913fb54b762da761
IcedFire34 <omar.faruk.bozkaya@gmail.com>
2024-05-19 14:31:39 +0300

    Android izinleri düzeltildi, baglantı ve veri gönderimi eklendi

--- lib/bluetooth.dart
+++ lib/bluetooth.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-class BluetoothCls extends StatelessWidget {
+class BluetoothCls extends StatefulWidget {
+  static BluetoothConnection? connection;
+
   const BluetoothCls({super.key});
+
+  @override
+  _BluetoothClsState createState() => _BluetoothClsState();
+}
+
+class _BluetoothClsState extends State<BluetoothCls> {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice? _device;
+  bool _connected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    FlutterBluetoothSerial.instance.onStateChanged().listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    FlutterBluetoothSerial.instance.getBondedDevices().then((List<BluetoothDevice> devices) {
+      setState(() {
+        _devicesList = devices;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    if (_connected) {
+      BluetoothCls.connection?.close();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bluetooth Cihazları')),
-      body: const Center(child: Text('Bluetooth sayfası')),
+      body: ListView(
+        children: _devicesList.map((device) {
+          return ListTile(
+            title: Text(device.name ?? ''),
+            subtitle: Text(device.address),
+            onTap: () async {
+              try {
+                BluetoothConnection connection = await BluetoothConnection.toAddress(device.address);
+                setState(() {
+                  BluetoothCls.connection = connection;
+                  _connected = true;
+                });
+                Navigator.pop(context);
+              } catch (e) {
+                setState(() {
+                  _connected = false;
+                });
+              }
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
--- lib/verigirisi.dart
+++ lib/verigirisi.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 
 class VeriGirisi extends StatelessWidget {
-  const VeriGirisi({super.key});
+  final BluetoothConnection? bluetoothConnection;
+
+  const VeriGirisi({Key? key, this.bluetoothConnection}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController hizController = TextEditingController();
+    final TextEditingController yonController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Veri Girişi')),
-      body: const Center(child: Text('Sıcaklık, yön,hız bilgileri buraya girilecek')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: hizController,
+              decoration: const InputDecoration(labelText: 'Hız'),
+            ),
+            TextField(
+              controller: yonController,
+              decoration: const InputDecoration(labelText: 'Yön'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String hiz = hizController.text;
+                String yon = yonController.text;
+
+                if (bluetoothConnection != null && bluetoothConnection!.isConnected) {
+                  bluetoothConnection!.output.add(Uint8List.fromList(utf8.encode('$hiz,$yon\n')));
+                }
+              },
+              child: const Text('Gönder'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
